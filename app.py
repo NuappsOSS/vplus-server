642de63e969ea78f6867b2e0bf9a02e089e514d8
ex0dus-0x <ex0dus@codemuch.tech>
2018-11-12 18:45:08 -0500

    Revamped design

--- app.py
+++ app.py
@@ -1,33 +1,40 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
+
 from flask import Flask, render_template, redirect, url_for, flash, request, session
 from flask_wtf import FlaskForm
 from flask_googlemaps import GoogleMaps
 from flask_qrcode import QRcode
 from wtforms import StringField, TextAreaField
-from wtforms.validators import DataRequired #... and other necessary validators
+from wtforms.validators import DataRequired
 from flask_material import Material
+from firebase_admin import credentials, db
 
 import os
 import urllib
 import simplejson
 import firebase_admin
-from firebase_admin import credentials, db
 
+# consts
+GOOGLEMAPS_API = 'http://maps.googleapis.com/maps/api/geocode/json?'
+FIREBASE_URL = "https://veiplus-96fa7.firebaseio.com/"
+
+# initialize Firebase with credentials
 cred = credentials.Certificate('private/serviceAccountKey.json')
 default_app = firebase_admin.initialize_app(cred, {
-    'databaseURL' : "https://veiplus-96fa7.firebaseio.com/"
+    'databaseURL' : FIREBASE_URL
 })
 
-
+# initialize Flask
 app = Flask(__name__)
-
 app.secret_key = os.urandom(100)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['GOOGLEMAPS_KEY'] = "AIzaSyBddL64NmxF6-8GVRl4gUvJDCXFf0KmB0w"
 
+# initialize Flask extensions
 GoogleMaps(app)
 QRcode(app)
 Material(app)
+
 
 def searchQuery(query, reference):
     search = []
@@ -36,8 +43,8 @@ def searchQuery(query, reference):
     for result in queries:
         if result[1] == query:
             search.append(result)
-
     return search
+
 
 def employeeTranverse(companyName):
     employees = []
@@ -46,7 +53,6 @@ def employeeTranverse(companyName):
     for employee in search:
         if employee[7] == companyName:
             employees.append(employee)
-
     return employees
 
 
@@ -61,8 +67,6 @@ def getCompanies():
         companies.append(company)
 
     return companies
-
-googleGeocodeUrl = 'http://maps.googleapis.com/maps/api/geocode/json?'
 
 def get_coordinates(query):
     query = str(query).encode('utf-8')
@@ -82,49 +86,67 @@ def get_coordinates(query):
         print query, "<no results>"
     return latitude, longitude
 
-# Redirect to 404
+
 @app.errorhandler(404)
 def page_not_found(e):
+    """
+    Redirect to 404.html on 404 error
+    """
     return render_template('404.html'), 404
 
-@app.route('/index')
-def redirect_to_index():
-    return redirect(url_for('index'))
 
 @app.route('/privacypolicy')
 def privacy_policy():
+    """
+    Render Privacy Policy page
+    """
     return render_template('privacypolicy.html',
                         title="Privacy Policy")
 
-# Main Page
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    featured = "Nuapps"
 
+@app.route('/about')
+def about():
+    """
+    Render about page
+    """
+    return render_template('about.html', title="About VEPlus")
+
+
+@app.route('/')
+def home():
+    return render_template('index.html', title='Welcome!')
+
+
+@app.route('/main', methods=['GET', 'POST'])
+def main_page():
+    """
+    Initialize index with featured company
+    """
+ 
+    # TODO: randomize featured company
+    featured = "Matchwear"
     featuredCompany = searchQuery(featured, "companies/")
-
+    
+    # on POST, redirect to /search
     if request.method == "POST":
         return redirect(url_for("search", query=request.form["searchQuery"]))
 
-    return render_template('index.html',
+    return render_template('main.html',
                         title="Welcome!",
                         featuredCompany=featuredCompany)
 
-# About Page
-@app.route('/about')
-def about():
-    return render_template('about.html', title="About VEPlus")
 
 @app.route('/list')
 def listCompanies():
+    """
+    Render a page with list of companies from Firebase
+    """
     companies = getCompanies()
     return render_template('list.html',
                             title="Available Companies",
                             companies=companies)
 
 
-
-# Search route for companies
 @app.route('/search/?q=<query>', methods=['GET', 'POST'])
 def search(query):
 
@@ -138,7 +160,6 @@ def search(query):
                     searchEmployee=searchEmployee)
 
 
-# Profile route for employees
 @app.route('/profile/<employee_name>', methods=['GET'])
 def profile(employee_name):
     search = searchQuery(employee_name, "employees/")
@@ -150,7 +171,7 @@ def profile(employee_name):
                 title=employee_name,
                 employee=search)
 
-# Profile route for company
+
 @app.route('/company/<company_name>', methods=['GET'])
 def company(company_name):
 
